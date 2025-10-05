194473861737f971cd6f8052c21ec26d4a1aebaf
Mohamed Kamaleldin <mohammed.kamaleldin@innosoft.com>
2025-10-05 12:29:55 +0300

    Assignment Completed

--- 5-3-react-event-handling/src/components/TaskApp.jsx
+++ 5-3-react-event-handling/src/components/TaskApp.jsx
@@ -2,19 +2,30 @@ import React, { useState } from "react";
 import TaskList from "./TaskList";
 
 export default function TaskApp() {
+
+  const [text, setText] = useState("");
+
+  const [tasks, setTasks] = useState([]);
   
   const handleSubmit = () => {
-   
+
+    if(text.trim !== ""){
+      setTasks(prev => [...prev, { id: Date.now(), text }]);
+      setText("");
+    }
   };
 
   
   const handleDelete = (id) => {
     // TODO: filter tasks by id to remove the clicked one
+           setTasks(prev => prev.filter(t => t.id !== id));
+
   };
 
   
   const handleClearAll = () => {
     // TODO: set tasks to empty array
+       setTasks([]);
   };
 
   return (
@@ -25,8 +36,8 @@ export default function TaskApp() {
           type="text"
           placeholder="Type a task..."
           className="input"
-          // TODO: value={text}
-          // TODO: onChange={(e) => setText(e.target.value)}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
           onKeyDown={(e) => {
             if (e.key === "Enter") handleSubmit();
           }}
@@ -38,7 +49,7 @@ export default function TaskApp() {
 
       {/*Render Task List and Enable Delete */}
       {/*Pass tasks and onDelete */}
-      <TaskList /* tasks={tasks} onDelete={handleDelete} */ />
+      <TaskList tasks={tasks} onDelete={handleDelete} />
 
       {/*Clear All */}
       <div className="footerRow">
--- 5-3-react-event-handling/src/components/TaskList.jsx
+++ 5-3-react-event-handling/src/components/TaskList.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import TaskItem from "./TaskItem";
+import { useState } from "react";
+
+
 
 export default function TaskList({ tasks, onDelete }) {
+
   return (
     <ul className="list">
-      {/* Task 2 – Display Placeholder if No Tasks Yet */}
+      {/* Placeholder */}
 
+        {tasks.length == 0 ? (
+        <li className="item item--placeholder">No tasks yet</li>
+      ) : (
+        tasks.map((task) => (
+          <TaskItem key={task.id} id={task.id} text={task.text} onDelete={() => onDelete(task.id)} />
+        ))
+      )}
       {/* Task 2 & 3 – Map tasks to TaskItem */}
       
     </ul>
